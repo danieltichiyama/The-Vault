52132905d3a83bc42e3d40b33c2973a5bd042316
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-07-02 22:05:40 -1000

    done

--- vault.js
+++ vault.js
@@ -1,4 +1,27 @@
 'use strict';
 module.exports = function() {
   
+    let myVault = {};
+
+    function setValue (key, value){
+        myVault[key] = value;
+        // console.log(myVault);
+    };
+
+    function getValue (key){
+        if (key ===null){
+            return null;
+        }
+        if (!(key in myVault)){
+            return null;
+        }
+        return myVault[key];
+
+    };
+
+
+    return {
+        setValue: setValue,
+        getValue: getValue,
+    }
 };
